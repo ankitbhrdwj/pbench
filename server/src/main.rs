b2cc02e77c89b2464afd2fbbfb3ee728e625c333
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2019-06-11 19:22:32 -0600

    Fork multiple processes for network endpoints.
    Add a configuration file to enter the server IP-address and number of
    network points. The server reads this configuration file for initialization.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,39 +1,96 @@
-use std::net::{TcpListener, TcpStream, Shutdown};
+extern crate confy;
+extern crate nix;
+#[macro_use]
+extern crate serde_derive;
+
+use nix::unistd::{fork, ForkResult};
 use std::io::{Read, Write};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::net::{Shutdown, TcpListener, TcpStream};
+use std::str::FromStr;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MyConfig {
+    server: String,
+    ports: u64,
+}
+
+impl Default for MyConfig {
+    fn default() -> Self {
+        MyConfig {
+            server: "10.10.1.1".to_string(),
+            ports: 1,
+        }
+    }
+}
+
+fn load() -> MyConfig {
+    let config = confy::load("../config");
+    match config {
+        Ok(config) => config,
+        Err(err) => {
+            println!("{}. Taking default config.", err);
+            MyConfig::default()
+        }
+    }
+}
 
 fn handle_client(mut stream: TcpStream) {
     let mut data = [0 as u8; 128]; // TODO: Decide on the payload size.
     while match stream.read(&mut data) {
         Ok(size) => {
-	    println!("{}", size);
             stream.write(&data[0..size]).unwrap();
             true
-        },
+        }
 
         Err(_) => {
-            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            println!(
+                "An error occurred, terminating connection with {}",
+                stream.peer_addr().unwrap()
+            );
             stream.shutdown(Shutdown::Both).unwrap();
             false
         }
     } {}
 }
 
-fn main() {
-    let listener = TcpListener::bind("10.10.1.1:3333").unwrap();
-    println!("Server listening on port 3333");
+fn listen(listener: &TcpListener) {
     for stream in listener.incoming() {
-      match stream {
-          Ok(stream) => {
-              println!("New connection: {}", stream.peer_addr().unwrap());
-	      handle_client(stream);
-          }
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                handle_client(stream);
+            }
 
-          Err(e) => {
-              println!("Error: {}", e);
-          }
-      }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
     }
-    // close the socket server
-    drop(listener);
-    println!("Hello, world!");
+}
+
+fn main() {
+    let mut start_port = 1024;
+    let config = load();
+    let ip = Ipv4Addr::from_str(&config.server).unwrap();
+    println!("{}", config.ports);
+    for _i in 0..config.ports {
+        let socket_addr: SocketAddr = SocketAddr::new(IpAddr::V4(ip), start_port);
+        let listener = TcpListener::bind(socket_addr).unwrap();
+        match fork() {
+            Ok(ForkResult::Parent { child, .. }) => {
+                println!("New process with pid: {} for port {}", child, start_port);
+            }
+
+            Ok(ForkResult::Child) => {
+                listen(&listener);
+                drop(listener);
+            }
+
+            Err(_) => println!("Fork failed"),
+        }
+        start_port += 1;
+    }
+    println!("Initilization done!!!");
+    loop {}
 }
